94411d696f5a1edb9579697dae42efa05e846ab1
Hussain-Tanveer2003 <105233465+Hussain-Tanveer2003@users.noreply.github.com>
2022-05-17 04:46:47 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Todo from './Components/Todo/Todo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {useSelector} from "react-redux"
+function App() {
+
+  return (
+  <div>
+   <Todo/>
+  </div>
+  );
+}
+
+export default App;
